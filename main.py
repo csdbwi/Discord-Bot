c8050ecd9b8e8dc413f399801bca95dec11024f1
csdbwi <csdbwi@icloud.com>
2021-07-02 12:40:22 +0800

    Moderation

--- main.py
+++ main.py
@@ -1,11 +1,50 @@
 import discord
-from discordToken import TOKEN
+from discord.ext import commands
 
-client = discord.Client()
+client = commands.Bot(command_prefix='.', help_command=None)
+embed = discord.Embed()
 
-@client.event
-async def on_message(message):
-    if message.content == 'hello' or message.content == 'Hello':
-        await message.channel.send('Pog')
+#---------------------------------------------------------------------------
+
+@client.command()
+@commands.has_any_role('CSDBWI')
+async def clear(ctx, amount:int):
+    await ctx.channel.purge(limit=amount)
+
+#---------------------------------------------------------------------------
+
+@client.command()
+@commands.has_any_role('🔧 Moderator')
+async def ban(ctx, member:discord.Member, *, reason):
+    await member.ban(reason = reason)
+    await ctx.send(f'{ctx.author} banned {member}.')
+
+@client.command()
+@commands.has_any_role('🔧 Moderator')
+async def kick(ctx, member:discord.Member, *, reason):
+    await member.kick(reason = reason)
+    await ctx.send(f'{ctx.author} kicked {member}.')
+
+@client.command()
+@commands.has_any_role('🔧 Moderator')
+async def mute(ctx, member: discord.Member):
+    await member.add_roles(discord.utils.get(ctx.guild.roles, name='Muted'))
+
+@client.command()
+@commands.has_any_role('🔧 Moderator')
+async def unmute(ctx, member: discord.Member):
+    await member.remove_roles(discord.utils.get(ctx.guild.roles, name='Muted'))
+
+#---------------------------------------------------------------------------
+
+@client.command()
+async def report(ctx, member: discord.Member, *, reason):
+    report = client.get_channel(856870848212369448)
+    embed.add_field(name='User', value=member, inline=False)
+    embed.add_field(name='Reason', value=reason, inline=False)
+    embed.add_field(name='Reported by', value=ctx.author, inline=False)
+    await report.send(embed=embed)
+
+#---------------------------------------------------------------------------
 
 client.run(TOKEN)
